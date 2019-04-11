ae6707b36eded1a1a70fe58e3c94441fcbe571e1
Phil Carter <philiplicarter@gmail.com>
2019-04-11 17:26:33 -0400

    Update main view and add detail view.

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tasks/<int:task_id>/', views.detail, name='detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,25 @@
+from datetime import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader, Context, Template
+
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, today. You're at the tasks index.")
+    now = datetime.today()
+    todays_tasks = Task.objects.all()
+
+    # template = loader.get_template('tasks/index.html')
+    context = {
+        'datetime': now,
+        'todays_tasks': todays_tasks,
+    }
+
+    return render(request, 'tasks/index.html', context)
+
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'tasks/detail.html', {'task': task})
